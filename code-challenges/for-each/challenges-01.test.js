89b8d6e9a479cda5ef2beafa91272c75812cd52a
bomibear <bomibearly@gmail.com>
2019-07-03 23:06:06 -0700

    double checking to see if tests have been commented out

--- code-challenges/for-each/challenges-01.test.js
+++ code-challenges/for-each/challenges-01.test.js
@@ -35,7 +35,6 @@ const addValues = (arr, value) => {
 };
 
 const addNumbers = (num, arr, times, callback) => {
-  // Solution code here...
   for(let i = 0; i < times; i++) {
     callback(arr, num);
   }
